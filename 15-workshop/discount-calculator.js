46c761a567ca1c4d12155c6f710356596a3fac60
sarahmdht <sarahmdht@gmail.com>
2024-08-20 15:24:14 +0300

    requirements

---
+++ 15-workshop/discount-calculator.js
@@ -0,0 +1,11 @@
+// calculating cart discounts (from API)
+const customers = [
+    {id: 1, name: "Apple", price: 100, discount: 5, amount: 10},
+    {id: 2, name: "Orange", price: 80, discount: 10, amount: 5},
+    {id: 3, name: "Banana", price: 50, discount: 20, amount: 1},
+    {id: 4, name: "felfel", price: 200, discount: 7, amount: 5},
+    {id: 5, name: "watermelon", price: 150, discount: 3, amount: 2}
+];
+cost;
+discount
+totalCost;
